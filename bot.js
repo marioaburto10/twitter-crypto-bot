1ff487241acc5eff0ae42298de824e06b8c4872f
Mario Aburto <marioaburto10@gmail.com>
2018-05-02 01:23:21 -0400

    able to tweet prices for top 10 crypto coins from coinmarketcap

--- bot.js
+++ bot.js
@@ -8,38 +8,94 @@ var Twitter = new Twit(config);
 
 
 // run a request to the cryptopanic API 
-request("https://cryptopanic.com/api/posts/?auth_token=b3452c8af088c05e6ca151617bac9b9146c004b8&public=true", function(err, response, body) {
+// request("https://cryptopanic.com/api/posts/?auth_token=b3452c8af088c05e6ca151617bac9b9146c004b8&public=true", function(err, response, body) {
 
+//   // If the request is successful (i.e. if the response status code is 200)
+//   if (!err && response.statusCode === 200) {
+
+//     // Parse the body of the site and recover the data coming back
+//     // console.log("The data coming back is : " + JSON.parse(body).results);
+    
+//     // parse the results coming back and save them in a variable
+//     const results = JSON.parse(body).results[1]
+
+//     // storing article headline
+//     let newsHeadline = results.title;
+
+//     // storing article url
+//     let link = results.url;
+
+//     // creating a variable to store a coin ticker
+//     let ticker;
+
+//     // if the article has a coin ticker then store it
+//     if (results.currencies) {
+//       ticker = results.currencies[0].code;
+//     }
+
+//     console.log("headline : ", newsHeadline);
+
+
+//     // if a ticker exists, post a tweet with the ticker, if not then just post a tweet with the headline and url
+//     if (ticker) {
+//       // send a post request to twitter with the status being the news headline
+//       Twitter.post('statuses/update', { status: newsHeadline + " " + link + " " +  "$" + ticker + " #cryptonews" +  " #crypto"}, function(err, data, response) {
+
+//         // if there is an error, log the error
+//         if(err){
+//           console.log(err);
+//         }
+//         // if there is no error, log the data coming back
+//         else {
+//           console.log(data.text);
+//           console.log("------ YES ticker");
+//         }
+
+//       });
+//     }
+//     else {
+//       // send a post request to twitter with the status being the news headline
+//       Twitter.post('statuses/update', { status: newsHeadline + " " + link + " #cryptonews" +  " #crypto"}, function(err, data, response) {
+
+//         // if there is an error, log the error
+//         if(err){
+//           console.log(err);
+//         }
+//         // if there is no error, log the data coming back
+//         else {
+//           console.log(data.text);
+//           console.log("------ NO ticker");
+//         }
+
+//       });
+//     }
+
+//   }
+// });
+
+
+// send a request to the coinmarketcap API for the top 10 coins
+request("https://api.coinmarketcap.com/v2/ticker/?limit=10", function(err, response, body) {
   // If the request is successful (i.e. if the response status code is 200)
   if (!err && response.statusCode === 200) {
 
-    // Parse the body of the site and recover the data coming back
-    // console.log("The data coming back is : " + JSON.parse(body).results);
     
-    // parse the results coming back and save them in a variable
-    const results = JSON.parse(body).results[1]
+    // parse the results coming back and save them in a variable called data
+    const data = JSON.parse(body).data
+    // console.log(data);
 
-    // storing article headline
-    let newsHeadline = results.title;
+    // create a variable to store the tweet
+    let tweet = "LATEST PRICES FOR TOP 10 COINS: ";
 
-    // storing article url
-    let link = results.url;
-
-    // creating a variable to store a coin ticker
-    let ticker;
-
-    // if the article has a coin ticker then store it
-    if (results.currencies) {
-      ticker = results.currencies[0].code;
+    // iterate through the data object that hold the top 10 cryptocurrencies
+    for (var property in data) {
+      tweet = tweet + " $" + data[property].symbol + " =" + " " + data[property].quotes.USD.price + "USD | " ;
     }
 
-    console.log("headline : ", newsHeadline);
+    console.log(tweet);
 
-
-    // if a ticker exists, post a tweet with the ticker, if not then just post a tweet with the headline and url
-    if (ticker) {
       // send a post request to twitter with the status being the news headline
-      Twitter.post('statuses/update', { status: newsHeadline + " " + link + " " +  "$" + ticker + " #cryptonews" +  " #crypto"}, function(err, data, response) {
+      Twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
 
         // if there is an error, log the error
         if(err){
@@ -48,31 +104,15 @@ request("https://cryptopanic.com/api/posts/?auth_token=b3452c8af088c05e6ca151617
         // if there is no error, log the data coming back
         else {
           console.log(data.text);
-          console.log("------ YES ticker");
+          console.log("------ PRICES HAVE BEEN TWEETED");
         }
 
       });
-    }
-    else {
-      // send a post request to twitter with the status being the news headline
-      Twitter.post('statuses/update', { status: newsHeadline + " " + link + " #cryptonews" +  " #crypto"}, function(err, data, response) {
-
-        // if there is an error, log the error
-        if(err){
-          console.log(err);
-        }
-        // if there is no error, log the data coming back
-        else {
-          console.log(data.text);
-          console.log("------ NO ticker");
-        }
-
-      });
-    }
 
   }
-});
 
+
+});
 
 
 
