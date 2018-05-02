2a1037bc5946c55d147866dc447c983eef5436e0
Mario Aburto <marioaburto10@gmail.com>
2018-05-02 00:02:36 -0400

    set up request to cryptopanic API, retrieve a news headline and tweet it successfull

--- bot.js
+++ bot.js
@@ -1,22 +1,54 @@
 //Require dependencies
 var Twit = require('twit');
-var config = require('./config.js');
-console.log(config);
+var config = require('./config.js'); 
+var request = require("request");
 
 //putting our configuration details in twitter
 var Twitter = new Twit(config);
-console.log(Twitter);
-console.log('-----------');
 
-
-// Creating retweet bot, that will find the latest crypto news. We will take params object that will take properties initialize by us.
-// In the above code, q is the required parameter which is used to store search query. It will search for tweets containing #crypto. Where count is the number of tweets, result_type:’recent’ will return the most recent results and lang:’en’ returns English results.
+// q is the required parameter which is used to store search query. It will search for tweets containing #crypto. Where count is the number of tweets, result_type:’recent’ will return the most recent results and lang:’en’ returns English results.
 var params = {
 q: '#crypto',
 count: 10,
 result_type: 'recent',
 lang: 'en'
 }
+
+
+// run a request to the cryptopanic API 
+request("https://cryptopanic.com/api/posts/?auth_token=b3452c8af088c05e6ca151617bac9b9146c004b8&public=true", function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover the data coming back
+    // console.log("The data coming back is : " + JSON.parse(body).results);
+    
+    // save the headline
+    var newsHeadline = JSON.parse(body).results[1].title;
+
+    // send a post request to twitter with the status being the news headline
+    Twitter.post('statuses/update', { status: newsHeadline }, function(err, data, response) {
+
+      // if there is an error, log the error
+      if(err){
+        console.log(err);
+      }
+      // if there is no error, log the data coming back
+      else {
+        console.log(data);
+      }
+
+    });
+
+
+  }
+});
+
+
+
+
+
 
 // attach the search parameters into the get request to find tweets
 // we make a get request to search/tweets and pass in our search .
