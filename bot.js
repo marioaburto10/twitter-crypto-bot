a98a1086713067dc443c1ff8b5677e5413d7338d
Mario Aburto <marioaburto10@gmail.com>
2018-07-25 00:32:39 -0400

    fixed function tweetLatestPrices() so that the coins are tweeted in order by rank and did some string formatting

--- bot.js
+++ bot.js
@@ -1,14 +1,13 @@
 //Require dependencies
-// var config = require('./config.js');
-var Twit = require('twit');
-var request = require("request");
+const Twit = require('twit');
+const request = require("request");
 //local twitter config file or heroku config
 let config;
 //local cryptopanic api config file or heroku config
 let cryptopanicApiKey;
 
 // use this config when deploying to Heroku
-var herokuConfig = {
+const herokuConfig = {
   consumer_key: process.env['consumer_key'],
   consumer_secret: process.env['consumer_secret'],
   access_token: process.env['access_token'],
@@ -30,25 +29,25 @@ else{
 
 
 //putting our configuration details in twitter
-var Twitter = new Twit(config);
+const Twitter = new Twit(config);
 
 //upon running the bot, tweet the latest top ten crypto prices
 tweetLatestPrices();
 
 
-// tweet latest crypto news every 29 mins
+// // tweet latest crypto news every 29 mins
 setInterval(tweetLatestCryptoNews, 1000*60*29);
 
-// tweet latest prices once an hour
+// // tweet latest prices once an hour
 setInterval(tweetLatestPrices, 1000*60*60);
 
-// retweet the top two most recent crypto tweets every 3.9 hours
+// // retweet the top two most recent crypto tweets every 3.9 hours
 setInterval(retweetMostRecentTweets, 1000*60*60*3.9);
 
 
 
 //experimenting with the ccxt package
-// var ccxt = require ('ccxt');
+// const ccxt = require ('ccxt');
 // console.log (ccxt.exchanges); // print all available exchanges
 
 
@@ -124,7 +123,6 @@ function tweetLatestCryptoNews() {
 }
 
 
-
 // function that will tweet the latest top 10 crypto prices
 function tweetLatestPrices() {
   // send a request to the coinmarketcap API for the top 10 coins
@@ -132,22 +130,51 @@ function tweetLatestPrices() {
     // If the request is successful (i.e. if the response status code is 200)
     if (!err && response.statusCode === 200) {
 
-      
+      let topTenCoins = [];
+      let orderedArr = [1,2,3,4,5,6,7,8,9,10]
+ 
       // parse the results coming back and save them in a variable called data
       const data = JSON.parse(body).data
       // console.log(data);
 
+      for (let property in data) {
+        let rank = data[property].rank;
+        let symbol = data[property].symbol;
+        let price = data[property].quotes.USD.price;
+        topTenCoins.push({
+          "rank": rank,
+          "symbol": symbol,
+          "price": price
+        });
+      }
+
+
+      let orderedTopTenCoins = [];
+      for (let i = 0; i < orderedArr.length; i++) {
+        // console.log("first index ", orderedArr[i]);
+        for (let k = 0; k < topTenCoins.length; k++) {
+          // console.log(ranks[k].rank);
+          if (orderedArr[i] == topTenCoins[k].rank) {
+            // console.log(ranks[k].rank);
+            orderedTopTenCoins.push(topTenCoins[k]);
+          }
+        }
+      }
+      // console.log("New ordered" , orderedTopTenCoins);
+      
       // create a variable to store the tweet
-      let tweet = "LATEST PRICES FOR TOP 10 COINS: ";
+      let tweet = "LATEST PRICES FOR TOP 10 CRYPTOCURRENCIES (USD): \n";
+      
 
       // iterate through the data object that hold the top 10 cryptocurrencies
-      for (var property in data) {
-        tweet = tweet + " $" + data[property].symbol + " =" + " " + data[property].quotes.USD.price + "USD | " ;
+      for (let i = 0; i < orderedTopTenCoins.length; i++) {
+        // console.log(orderedTopTenCoins[i].rank, orderedTopTenCoins[i].symbol, orderedTopTenCoins[i].price)
+        tweet = tweet + orderedTopTenCoins[i].rank + ".) $" + orderedTopTenCoins[i].symbol + " = $" + orderedTopTenCoins[i].price + "\n" ;
       }
 
       console.log(tweet);
 
-        // send a post request to twitter with the status being the news headline
+      //   // send a post request to twitter with the status being the news headline
         Twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
 
           // if there is an error, log the error
@@ -174,7 +201,7 @@ function tweetLatestPrices() {
 function retweetMostRecentTweets() {
   // q is the required parameter which is used to store search query. It will search for tweets containing #crypto. Where count is the number of tweets, result_type:’recent’ will return the most recent results and lang:’en’ returns English results.
   // this will be passed in the get request to Twitter
-  var params = {
+  let params = {
     q: '#crypto',
     count: 2,
     result_type: 'recent',
@@ -210,3 +237,4 @@ function retweetMostRecentTweets() {
     }
   });
 }
+
