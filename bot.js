9756d1ac94efac43716a91a12c9c72d71eadc400
Mario Aburto <marioaburto10@gmail.com>
2018-07-28 13:55:43 -0400

    created a new function that will tweet global data and btc data every 8 hours

--- bot.js
+++ bot.js
@@ -35,14 +35,17 @@ const Twitter = new Twit(config);
 tweetLatestPrices();
 
 
-// // tweet latest crypto news every 29 mins
-setInterval(tweetLatestCryptoNews, 1000*60*29);
+// tweet latest crypto news every 6 mins
+setInterval(tweetLatestCryptoNews, 1000*60*6);
 
-// // tweet latest prices once an hour
+// tweet latest prices once an hour
 setInterval(tweetLatestPrices, 1000*60*60);
 
-// // retweet the top two most recent crypto tweets every 3.9 hours
+// retweet the top two most recent crypto tweets every 3.9 hours
 setInterval(retweetMostRecentTweets, 1000*60*60*3.9);
+
+// tweet global market data and btc data 7.9 hours
+setInterval(tweetMarketAndBTCData, 1000*60*60*7.9);
 
 
 
@@ -50,7 +53,10 @@ setInterval(retweetMostRecentTweets, 1000*60*60*3.9);
 // const ccxt = require ('ccxt');
 // console.log (ccxt.exchanges); // print all available exchanges
 
-
+// function that will format a string number to add commas -- ex. 8346 becomes 8,346
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 // function that will tweet the latest news article headline and url
 function tweetLatestCryptoNews() {
@@ -141,6 +147,7 @@ function tweetLatestPrices() {
         let rank = data[property].rank;
         let symbol = data[property].symbol;
         let price = data[property].quotes.USD.price;
+        price = numberWithCommas(price);
         topTenCoins.push({
           "rank": rank,
           "symbol": symbol,
@@ -165,7 +172,6 @@ function tweetLatestPrices() {
       // create a variable to store the tweet
       let tweet = "LATEST PRICES FOR TOP 10 CRYPTOCURRENCIES (USD): \n";
       
-
       // iterate through the data object that hold the top 10 cryptocurrencies
       for (let i = 0; i < orderedTopTenCoins.length; i++) {
         // console.log(orderedTopTenCoins[i].rank, orderedTopTenCoins[i].symbol, orderedTopTenCoins[i].price)
@@ -195,6 +201,108 @@ function tweetLatestPrices() {
   });
 }
 
+// function that will tweet Bitcoin's and the marketcap's data
+function tweetMarketAndBTCData() {
+  // send a request to the coinmarketcap API for the top 10 coins
+  request("https://api.coinmarketcap.com/v2/ticker/1/", function(err, response, body) {
+    let BTCpercent_change_1h;
+    let BTCpercent_change_24h;
+    let BTCpercent_change_7d;
+
+    // If the request is successful (i.e. if the response status code is 200)
+    if (!err && response.statusCode === 200) {
+ 
+      // parse the results coming back and save them in a variable called data
+      const data = JSON.parse(body).data
+
+      let BTCprice = data.quotes.USD.price;
+      BTCprice = numberWithCommas(BTCprice);
+      let BTCvolume24hrs = data.quotes.USD.volume_24h;
+      BTCvolume24hrs = numberWithCommas(BTCvolume24hrs);
+      let BTCmarketcap = data.quotes.USD.market_cap;
+      BTCmarketcap = numberWithCommas(BTCmarketcap);
+      BTCpercent_change_1h = String(data.quotes.USD.percent_change_1h);
+      BTCpercent_change_24h = String(data.quotes.USD.percent_change_24h);
+      BTCpercent_change_7d = String(data.quotes.USD.percent_change_7d);
+
+
+      // If the change is not negative, add a + sign in front of the price change
+      if (BTCpercent_change_1h[0] !== "-") {
+        BTCpercent_change_1h = "+" + BTCpercent_change_1h;
+      }
+
+      if (BTCpercent_change_24h[0] !== "-") {
+        BTCpercent_change_24h = "+" + BTCpercent_change_24h;
+      }
+
+      if (BTCpercent_change_7d[0] !== "-") {
+        BTCpercent_change_7d = "+" + BTCpercent_change_7d;
+      }
+
+
+      request("https://api.coinmarketcap.com/v2/global/", function(err, response, body) {
+        let total_market_cap;
+        if (!err && response.statusCode === 200) {
+          const data = JSON.parse(body).data
+
+          let BTC_marketcap_percentage = data.bitcoin_percentage_of_market_cap;
+          total_market_cap = data.quotes.USD.total_market_cap;
+          total_market_cap = numberWithCommas(total_market_cap);
+          total_market_cap = total_market_cap.split(",");
+
+          if (total_market_cap.length == 5) {
+            total_market_cap = total_market_cap[0] + " Trillion";
+
+          } else if (total_market_cap.length == 4) {
+            total_market_cap = total_market_cap[0] + " Billion";
+
+          } else if (total_market_cap.length == 3) {
+            total_market_cap = total_market_cap[0] + " Million";
+          }
+
+
+          let total_volume_24h = data.quotes.USD.total_volume_24h;
+          total_volume_24h = numberWithCommas(total_volume_24h);
+          // console.log(total_volume_24h);
+
+          let tweet = "GLOBAL MARKET DATA (USD):\n" +
+            "Market Cap: $" + total_market_cap + "\n" +
+            "24Hr Volume: $" + total_volume_24h + "\n" +
+            "BTC Dominance: " + BTC_marketcap_percentage +  "%\n" +
+            "\n" + 
+            "BTC DATA (USD):\n" +
+            "Price: $" + BTCprice + "\n" +
+            "Market Cap: $" + BTCmarketcap + "\n" +
+            "24Hr Volume: $" + BTCvolume24hrs + "\n" +
+            "Percent Change 1Hr: " + BTCpercent_change_1h + "%\n" +
+            "Percent Change 24Hr: " + BTCpercent_change_24h + "%\n" +
+            "Percent Change 7d: " + BTCpercent_change_7d + "%"
+          ;
+
+          console.log(tweet);
+
+          // send a post request to twitter with the status being the news headline
+          Twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
+
+            // if there is an error, log the error
+            if(err){
+              console.log(err);
+            }
+            // if there is no error, log the data coming back
+            else {
+              console.log(data.text);
+              console.log("------ PRICES HAVE BEEN TWEETED");
+            }
+
+          });
+        }
+      })
+
+    }
+
+
+  });
+}
 
 
 // function that will find the 2 most popular tweets on crypto and retweet them
