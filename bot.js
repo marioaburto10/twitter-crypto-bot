b56303bc69e4e39904a09c153fa296a53e1a96f0
Mario Aburto <marioaburto10@gmail.com>
2018-08-02 00:24:51 -0400

    fixed excess demimal places in prices, removed 1hr prices

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 //Require dependencies
 const Twit = require('twit');
 const request = require("request");
+
+global.fetch = require('node-fetch')
+const cc = require('cryptocompare')
+
 //local twitter config file or heroku config
 let config;
 //local cryptopanic api config file or heroku config
@@ -32,20 +36,20 @@ else{
 const Twitter = new Twit(config);
 
 //upon running the bot, tweet the latest top ten crypto prices
-tweetLatestPrices();
+// tweetLatestPrices();
 
 
-// tweet latest crypto news every 6 mins
-setInterval(tweetLatestCryptoNews, 1000*60*6);
+// tweet latest crypto news every 29 mins
+// setInterval(tweetLatestCryptoNews, 1000*60*29);
 
 // tweet latest prices once an hour
-setInterval(tweetLatestPrices, 1000*60*60);
+// setInterval(tweetLatestPrices, 1000*60*60);
 
 // retweet the top two most recent crypto tweets every 3.9 hours
-setInterval(retweetMostRecentTweets, 1000*60*60*3.9);
+// setInterval(retweetMostRecentTweets, 1000*60*60*3.9);
 
 // tweet global market data and btc data 7.9 hours
-setInterval(tweetMarketAndBTCData, 1000*60*60*7.9);
+// setInterval(tweetMarketAndBTCData, 1000*60*60*7.9);
 
 
 
@@ -147,23 +151,67 @@ function tweetLatestPrices() {
         let rank = data[property].rank;
         let symbol = data[property].symbol;
         let price = data[property].quotes.USD.price;
-
-        if (symbol == 'BTC') {
+      
+        if (String(price)[0] !== '0') {
+    
           topTenCoins.push({
             "rank": rank,
             "symbol": symbol,
-            "price": numberWithCommas(price)
+            "price": numberWithCommas(price.toFixed(2))
           });
+
         } else {
+          console.log("YES beginning with 0")
+      
           topTenCoins.push({
             "rank": rank,
             "symbol": symbol,
-            "price": price
+            "price": price.toFixed(4)
           });
         }
 
+
+
         
       }
+
+      console.log(topTenCoins);
+
+      // for (let k = 0; k < topTenCoins.length; k++) {
+      //     // if (topTenCoins[k].symbol == "MIOTA") {
+      //     //   // Passing a single pair of currencies:
+      //     //   cc.price("IOTA", 'USD')
+      //     //   .then(prices => {
+      //     //     console.log(prices)
+      //     //     // -> { USD: 1100.24 }
+      //     //   })
+      //     //   .catch(console.error)
+      //     // } else {
+      //     //   // Passing a single pair of currencies:
+      //     //   cc.price(topTenCoins[k].symbol, 'USD')
+      //     //   .then(prices => {
+      //     //     console.log(prices)
+      //     //     // -> { USD: 1100.24 }
+      //     //   })
+      //     //   .catch(console.error)
+      //     // }
+
+        
+          
+      // }
+
+      // request("http://coincap.io/front", function(err, response, body) {
+      //   if (!err && response.statusCode === 200) {
+      //     const data = JSON.parse(body)
+
+      //       // console.log(data);
+      //       for (var i = 0; i < 10; i++) {
+      //         console.log(data[i]);
+      //       }
+      //   }
+
+
+      // })
 
 
       let orderedTopTenCoins = [];
@@ -177,7 +225,7 @@ function tweetLatestPrices() {
           }
         }
       }
-      // console.log("New ordered" , orderedTopTenCoins);
+
       
       // create a variable to store the tweet
       let tweet = "LATEST PRICES FOR TOP 10 CRYPTOCURRENCIES (USD): \n";
@@ -211,11 +259,11 @@ function tweetLatestPrices() {
   });
 }
 
+tweetMarketAndBTCData();
 // function that will tweet Bitcoin's and the marketcap's data
 function tweetMarketAndBTCData() {
   // send a request to the coinmarketcap API for the top 10 coins
   request("https://api.coinmarketcap.com/v2/ticker/1/", function(err, response, body) {
-    let BTCpercent_change_1h;
     let BTCpercent_change_24h;
     let BTCpercent_change_7d;
 
@@ -225,21 +273,43 @@ function tweetMarketAndBTCData() {
       // parse the results coming back and save them in a variable called data
       const data = JSON.parse(body).data
 
-      let BTCprice = data.quotes.USD.price;
+      let BTCprice = data.quotes.USD.price.toFixed(2);
       BTCprice = numberWithCommas(BTCprice);
       let BTCvolume24hrs = data.quotes.USD.volume_24h;
-      BTCvolume24hrs = numberWithCommas(BTCvolume24hrs);
+      BTCvolume24hrs = numberWithCommas(BTCvolume24hrs.toFixed(0));
+      BTCvolume24hrs = BTCvolume24hrs.split(",");
+
       let BTCmarketcap = data.quotes.USD.market_cap;
       BTCmarketcap = numberWithCommas(BTCmarketcap);
-      BTCpercent_change_1h = String(data.quotes.USD.percent_change_1h);
+      BTCmarketcap = BTCmarketcap.split(",")
+
       BTCpercent_change_24h = String(data.quotes.USD.percent_change_24h);
       BTCpercent_change_7d = String(data.quotes.USD.percent_change_7d);
 
+      if (BTCvolume24hrs.length == 5 ) {
+        BTCvolume24hrs = BTCvolume24hrs[0] + " Trillion";
+
+      } else if (BTCvolume24hrs.length == 4) {
+        BTCvolume24hrs = BTCvolume24hrs[0] + " Billion";
+
+      } else if (BTCvolume24hrs.length == 3) {
+        BTCvolume24hrs = BTCvolume24hrs[0] + " Million";
+      }
+
+
+
+      if (BTCmarketcap.length == 5 ) {
+        BTCmarketcap = BTCmarketcap[0] + " Trillion";
+
+      } else if (BTCmarketcap.length == 4) {
+        BTCmarketcap = BTCmarketcap[0] + " Billion";
+
+      } else if (BTCmarketcap.length == 3) {
+        BTCmarketcap = BTCmarketcap[0] + " Million";
+      }
+
 
       // If the change is not negative, add a + sign in front of the price change
-      if (BTCpercent_change_1h[0] !== "-") {
-        BTCpercent_change_1h = "+" + BTCpercent_change_1h;
-      }
 
       if (BTCpercent_change_24h[0] !== "-") {
         BTCpercent_change_24h = "+" + BTCpercent_change_24h;
@@ -260,7 +330,7 @@ function tweetMarketAndBTCData() {
           total_market_cap = numberWithCommas(total_market_cap);
           total_market_cap = total_market_cap.split(",");
 
-          if (total_market_cap.length == 5) {
+          if (total_market_cap.length == 5 ) {
             total_market_cap = total_market_cap[0] + " Trillion";
 
           } else if (total_market_cap.length == 4) {
@@ -271,40 +341,95 @@ function tweetMarketAndBTCData() {
           }
 
 
-          let total_volume_24h = data.quotes.USD.total_volume_24h;
+          let total_volume_24h = data.quotes.USD.total_volume_24h.toFixed(0);
           total_volume_24h = numberWithCommas(total_volume_24h);
-          // console.log(total_volume_24h);
+          total_volume_24h = total_volume_24h.split(",");
 
-          let tweet = "GLOBAL MARKET DATA (USD):\n" +
-            "Market Cap: $" + total_market_cap + "\n" +
-            "24Hr Volume: $" + total_volume_24h + "\n" +
-            "BTC Dominance: " + BTC_marketcap_percentage +  "%\n" +
-            "\n" + 
-            "BTC DATA (USD):\n" +
-            "Price: $" + BTCprice + "\n" +
-            "Market Cap: $" + BTCmarketcap + "\n" +
-            "24Hr Volume: $" + BTCvolume24hrs + "\n" +
-            "Percent Change 1Hr: " + BTCpercent_change_1h + "%\n" +
-            "Percent Change 24Hr: " + BTCpercent_change_24h + "%\n" +
-            "Percent Change 7d: " + BTCpercent_change_7d + "%"
-          ;
+          if (total_volume_24h == 5 ) {
+            total_volume_24h = total_volume_24h[0] + " Trillion";
 
-          console.log(tweet);
+          } else if (total_volume_24h.length == 4) {
+            total_volume_24h = total_volume_24h[0] + " Billion";
+
+          } else if (total_volume_24h.length == 3) {
+            total_volume_24h = total_volume_24h[0] + " Million";
+          }
+
+
+          request("https://chasing-coins.com/api/v1/std/marketcap", function(err, response, body) {
+            if (!err && response.statusCode === 200) {
+              const data = JSON.parse(body)
+
+                // console.log(data);
+                MC24hr_change = data.change["24h"]
+
+                MC7d_change = data.change["7d"]
+
+                if (MC24hr_change[0] !== "-") {
+                  MC24hr_change = "+" + MC24hr_change;
+                }
+
+                if (MC7d_change[0] !== "-") {
+                  MC7d_change = "+" + MC7d_change;
+                }
+
+
+                let tweet = "GLOBAL MARKET DATA (USD):\n" +
+                  "Market Cap: $" + total_market_cap + "\n" +
+                  "Percent Change 24Hr: " + MC24hr_change + "%\n" +
+                  "Percent Change 7d: " + MC7d_change + "%\n" + 
+                  // "24Hr Volume: $" + total_volume_24h + "\n" +
+                  "BTC Dominance: " + BTC_marketcap_percentage +  "%\n" +
+                  "\n" + 
+                  "$BTC DATA (USD):\n" +
+                  "Price: $" + BTCprice + "\n" +
+                  "Market Cap: $" + BTCmarketcap + "\n" +
+                  // "24Hr Volume: $" + BTCvolume24hrs + "\n" +
+                  "Percent Change 24Hr: " + BTCpercent_change_24h + "%\n" +
+                  "Percent Change 7d: " + BTCpercent_change_7d + "%"
+                ;
+
+                console.log(tweet);
+
+                // send a post request to twitter with the status being the news headline
+                Twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
+
+                  // if there is an error, log the error
+                  if(err){
+                    console.log(err);
+                  }
+                  // if there is no error, log the data coming back
+                  else {
+                    console.log(data.text);
+                    console.log("------ PRICES HAVE BEEN TWEETED");
+                  }
+
+                });
+
+            }
+          });
+
+
+
+
+       
+
+          
 
           // send a post request to twitter with the status being the news headline
-          Twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
+          // Twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
 
-            // if there is an error, log the error
-            if(err){
-              console.log(err);
-            }
-            // if there is no error, log the data coming back
-            else {
-              console.log(data.text);
-              console.log("------ PRICES HAVE BEEN TWEETED");
-            }
+          //   // if there is an error, log the error
+          //   if(err){
+          //     console.log(err);
+          //   }
+          //   // if there is no error, log the data coming back
+          //   else {
+          //     console.log(data.text);
+          //     console.log("------ PRICES HAVE BEEN TWEETED");
+          //   }
 
-          });
+          // });
         }
       })
 
